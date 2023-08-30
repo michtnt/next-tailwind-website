0709f66ec5aaae4afbcfeeb4d5f8f09d366e340d
michtnt <michelletanotoo@gmail.com>
2023-08-30 21:23:43 +1000

    feat: rename file

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -11,7 +11,7 @@ type LayoutProps = {
   home?: boolean;
 };
 
-export default function Layout({ children, home = false }: LayoutProps) {
+function Layout({ children, home = false }: LayoutProps) {
   return (
     <div className="container flex-1 mx-auto max-w-xl pr-4 mt-12 mr-auto mb-24 space-y-4">
       <Head>
