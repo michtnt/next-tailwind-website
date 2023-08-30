6d5ec4e301a80449a591d09bbf8bf23b51f56681
michtnt <michelletanotoo@gmail.com>
2023-08-30 21:12:36 +1000

    feat: support OS preferred system

--- components/theme-switcher/ThemeSwitcher.tsx
+++ components/theme-switcher/ThemeSwitcher.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
 import dynamic from "next/dynamic";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import { MoonIcon, SunIcon } from "../icons";
 
 function ThemeSwitcherComponent() {
   const [theme, setTheme] = useLocalStorage("theme", "light");
+
+  useEffect(() => {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+      setTheme("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
 
   function setDarkTheme() {
     document.documentElement.classList.add("dark");
