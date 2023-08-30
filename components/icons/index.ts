02698e3e643d05362d0dff8adfb7c4b87b0adbad
michtnt <michelletanotoo@gmail.com>
2023-08-30 20:37:55 +1000

    feat: refactor icons

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,3 +1,5 @@
-export { default as GithubIcon } from "./github";
-export { default as LinkedInIcon } from "./linkedIn";
-export { default as InstagramIcon } from "./instagram";
+export { default as GithubIcon } from "./GithubIcon";
+export { default as LinkedInIcon } from "./LinkedinIcon";
+export { default as InstagramIcon } from "./InstagramIcon";
+export { default as SunIcon } from "./SunIcon";
+export { default as MoonIcon } from "./MoonIcon";
